1c1094e37c70169ba2c4a77a89c536c538b73a01
YahyaRamzy <89773568+YahyaRamzy@users.noreply.github.com>
2022-10-23 17:09:42 +0200

    new stuff

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const API_URL = '/users/new'
+const API_URL = '/users/'
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL + 'new', userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
